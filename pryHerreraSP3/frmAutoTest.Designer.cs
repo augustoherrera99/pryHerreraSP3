430843416843a594822e84d4e11fc23d0f993271
augustoherrera99 <augustofherrera@hotmail.com>
2022-10-18 23:55:56 -0300

    Agregar archivos de proyecto.

---
+++ pryHerreraSP3/frmAutoTest.Designer.cs
@@ -0,0 +1,300 @@
+﻿namespace pryHerreraSP3
+{
+    partial class frmAutoTest
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAutoTest));
+            this.lblNroTurno = new System.Windows.Forms.Label();
+            this.lblDominio = new System.Windows.Forms.Label();
+            this.lblAnioFabricacion = new System.Windows.Forms.Label();
+            this.lblTitular = new System.Windows.Forms.Label();
+            this.txtNroTurno = new System.Windows.Forms.TextBox();
+            this.txtDominio = new System.Windows.Forms.TextBox();
+            this.txtTitular = new System.Windows.Forms.TextBox();
+            this.mrcRegistro = new System.Windows.Forms.GroupBox();
+            this.numAnioFabricacion = new System.Windows.Forms.NumericUpDown();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.mrcConsulta = new System.Windows.Forms.GroupBox();
+            this.txtDominio6Caracteres = new System.Windows.Forms.TextBox();
+            this.lblDominio6Caracteres = new System.Windows.Forms.Label();
+            this.lblAnioMasAntiguo = new System.Windows.Forms.Label();
+            this.lblCantidadTurnos = new System.Windows.Forms.Label();
+            this.txtCantidadTurnos = new System.Windows.Forms.TextBox();
+            this.txtAnioMasAntiguo = new System.Windows.Forms.TextBox();
+            this.btnConsultar = new System.Windows.Forms.Button();
+            this.mrcRegistro.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numAnioFabricacion)).BeginInit();
+            this.mrcConsulta.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblNroTurno
+            // 
+            this.lblNroTurno.AutoSize = true;
+            this.lblNroTurno.Location = new System.Drawing.Point(19, 30);
+            this.lblNroTurno.Name = "lblNroTurno";
+            this.lblNroTurno.Size = new System.Drawing.Size(90, 13);
+            this.lblNroTurno.TabIndex = 0;
+            this.lblNroTurno.Text = "Número de Turno";
+            // 
+            // lblDominio
+            // 
+            this.lblDominio.AutoSize = true;
+            this.lblDominio.Location = new System.Drawing.Point(19, 60);
+            this.lblDominio.Name = "lblDominio";
+            this.lblDominio.Size = new System.Drawing.Size(45, 13);
+            this.lblDominio.TabIndex = 1;
+            this.lblDominio.Text = "Dominio";
+            // 
+            // lblAnioFabricacion
+            // 
+            this.lblAnioFabricacion.AutoSize = true;
+            this.lblAnioFabricacion.Location = new System.Drawing.Point(19, 91);
+            this.lblAnioFabricacion.Name = "lblAnioFabricacion";
+            this.lblAnioFabricacion.Size = new System.Drawing.Size(84, 13);
+            this.lblAnioFabricacion.TabIndex = 2;
+            this.lblAnioFabricacion.Text = "Año Fabricación";
+            // 
+            // lblTitular
+            // 
+            this.lblTitular.AutoSize = true;
+            this.lblTitular.Location = new System.Drawing.Point(19, 122);
+            this.lblTitular.Name = "lblTitular";
+            this.lblTitular.Size = new System.Drawing.Size(36, 13);
+            this.lblTitular.TabIndex = 3;
+            this.lblTitular.Text = "Titular";
+            // 
+            // txtNroTurno
+            // 
+            this.txtNroTurno.Location = new System.Drawing.Point(126, 27);
+            this.txtNroTurno.MaxLength = 5;
+            this.txtNroTurno.Name = "txtNroTurno";
+            this.txtNroTurno.Size = new System.Drawing.Size(70, 20);
+            this.txtNroTurno.TabIndex = 4;
+            this.txtNroTurno.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNroTurno_KeyPress);
+            // 
+            // txtDominio
+            // 
+            this.txtDominio.Location = new System.Drawing.Point(126, 57);
+            this.txtDominio.MaxLength = 7;
+            this.txtDominio.Name = "txtDominio";
+            this.txtDominio.Size = new System.Drawing.Size(100, 20);
+            this.txtDominio.TabIndex = 5;
+            this.txtDominio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDominio_KeyPress);
+            // 
+            // txtTitular
+            // 
+            this.txtTitular.Location = new System.Drawing.Point(126, 119);
+            this.txtTitular.MaxLength = 30;
+            this.txtTitular.Name = "txtTitular";
+            this.txtTitular.Size = new System.Drawing.Size(209, 20);
+            this.txtTitular.TabIndex = 7;
+            // 
+            // mrcRegistro
+            // 
+            this.mrcRegistro.Controls.Add(this.numAnioFabricacion);
+            this.mrcRegistro.Controls.Add(this.lblTitular);
+            this.mrcRegistro.Controls.Add(this.txtTitular);
+            this.mrcRegistro.Controls.Add(this.lblAnioFabricacion);
+            this.mrcRegistro.Controls.Add(this.txtNroTurno);
+            this.mrcRegistro.Controls.Add(this.lblDominio);
+            this.mrcRegistro.Controls.Add(this.lblNroTurno);
+            this.mrcRegistro.Controls.Add(this.txtDominio);
+            this.mrcRegistro.Location = new System.Drawing.Point(12, 12);
+            this.mrcRegistro.Name = "mrcRegistro";
+            this.mrcRegistro.Size = new System.Drawing.Size(354, 155);
+            this.mrcRegistro.TabIndex = 8;
+            this.mrcRegistro.TabStop = false;
+            this.mrcRegistro.Text = "Registro de Turnos";
+            // 
+            // numAnioFabricacion
+            // 
+            this.numAnioFabricacion.Location = new System.Drawing.Point(126, 89);
+            this.numAnioFabricacion.Maximum = new decimal(new int[] {
+            2022,
+            0,
+            0,
+            0});
+            this.numAnioFabricacion.Minimum = new decimal(new int[] {
+            1950,
+            0,
+            0,
+            0});
+            this.numAnioFabricacion.Name = "numAnioFabricacion";
+            this.numAnioFabricacion.Size = new System.Drawing.Size(58, 20);
+            this.numAnioFabricacion.TabIndex = 8;
+            this.numAnioFabricacion.Value = new decimal(new int[] {
+            1950,
+            0,
+            0,
+            0});
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(387, 25);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(83, 30);
+            this.btnRegistrar.TabIndex = 9;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(387, 72);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(83, 30);
+            this.btnSalir.TabIndex = 10;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // mrcConsulta
+            // 
+            this.mrcConsulta.Controls.Add(this.txtDominio6Caracteres);
+            this.mrcConsulta.Controls.Add(this.lblDominio6Caracteres);
+            this.mrcConsulta.Controls.Add(this.lblAnioMasAntiguo);
+            this.mrcConsulta.Controls.Add(this.lblCantidadTurnos);
+            this.mrcConsulta.Controls.Add(this.txtCantidadTurnos);
+            this.mrcConsulta.Controls.Add(this.txtAnioMasAntiguo);
+            this.mrcConsulta.Location = new System.Drawing.Point(12, 190);
+            this.mrcConsulta.Name = "mrcConsulta";
+            this.mrcConsulta.Size = new System.Drawing.Size(354, 120);
+            this.mrcConsulta.TabIndex = 11;
+            this.mrcConsulta.TabStop = false;
+            this.mrcConsulta.Text = "Estadísticas";
+            // 
+            // txtDominio6Caracteres
+            // 
+            this.txtDominio6Caracteres.Location = new System.Drawing.Point(218, 80);
+            this.txtDominio6Caracteres.Name = "txtDominio6Caracteres";
+            this.txtDominio6Caracteres.ReadOnly = true;
+            this.txtDominio6Caracteres.Size = new System.Drawing.Size(45, 20);
+            this.txtDominio6Caracteres.TabIndex = 5;
+            // 
+            // lblDominio6Caracteres
+            // 
+            this.lblDominio6Caracteres.AutoSize = true;
+            this.lblDominio6Caracteres.Location = new System.Drawing.Point(19, 83);
+            this.lblDominio6Caracteres.Name = "lblDominio6Caracteres";
+            this.lblDominio6Caracteres.Size = new System.Drawing.Size(186, 13);
+            this.lblDominio6Caracteres.TabIndex = 2;
+            this.lblDominio6Caracteres.Text = "Cantidad con dominio de 6 caracteres";
+            // 
+            // lblAnioMasAntiguo
+            // 
+            this.lblAnioMasAntiguo.AutoSize = true;
+            this.lblAnioMasAntiguo.Location = new System.Drawing.Point(19, 53);
+            this.lblAnioMasAntiguo.Name = "lblAnioMasAntiguo";
+            this.lblAnioMasAntiguo.Size = new System.Drawing.Size(104, 13);
+            this.lblAnioMasAntiguo.TabIndex = 1;
+            this.lblAnioMasAntiguo.Text = "Año del más Antiguo";
+            // 
+            // lblCantidadTurnos
+            // 
+            this.lblCantidadTurnos.AutoSize = true;
+            this.lblCantidadTurnos.Location = new System.Drawing.Point(19, 25);
+            this.lblCantidadTurnos.Name = "lblCantidadTurnos";
+            this.lblCantidadTurnos.Size = new System.Drawing.Size(100, 13);
+            this.lblCantidadTurnos.TabIndex = 0;
+            this.lblCantidadTurnos.Text = "Cantidad de Turnos";
+            // 
+            // txtCantidadTurnos
+            // 
+            this.txtCantidadTurnos.Location = new System.Drawing.Point(218, 22);
+            this.txtCantidadTurnos.Name = "txtCantidadTurnos";
+            this.txtCantidadTurnos.ReadOnly = true;
+            this.txtCantidadTurnos.Size = new System.Drawing.Size(45, 20);
+            this.txtCantidadTurnos.TabIndex = 3;
+            // 
+            // txtAnioMasAntiguo
+            // 
+            this.txtAnioMasAntiguo.Location = new System.Drawing.Point(218, 50);
+            this.txtAnioMasAntiguo.Name = "txtAnioMasAntiguo";
+            this.txtAnioMasAntiguo.ReadOnly = true;
+            this.txtAnioMasAntiguo.Size = new System.Drawing.Size(45, 20);
+            this.txtAnioMasAntiguo.TabIndex = 4;
+            // 
+            // btnConsultar
+            // 
+            this.btnConsultar.Location = new System.Drawing.Point(387, 199);
+            this.btnConsultar.Name = "btnConsultar";
+            this.btnConsultar.Size = new System.Drawing.Size(83, 30);
+            this.btnConsultar.TabIndex = 12;
+            this.btnConsultar.Text = "Consultar";
+            this.btnConsultar.UseVisualStyleBackColor = true;
+            this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
+            // 
+            // frmAutoTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(490, 318);
+            this.Controls.Add(this.btnConsultar);
+            this.Controls.Add(this.mrcConsulta);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.mrcRegistro);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmAutoTest";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Test";
+            this.Load += new System.EventHandler(this.frmAutoTest_Load);
+            this.mrcRegistro.ResumeLayout(false);
+            this.mrcRegistro.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numAnioFabricacion)).EndInit();
+            this.mrcConsulta.ResumeLayout(false);
+            this.mrcConsulta.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNroTurno;
+        private System.Windows.Forms.Label lblDominio;
+        private System.Windows.Forms.Label lblAnioFabricacion;
+        private System.Windows.Forms.Label lblTitular;
+        private System.Windows.Forms.TextBox txtNroTurno;
+        private System.Windows.Forms.TextBox txtDominio;
+        private System.Windows.Forms.TextBox txtTitular;
+        private System.Windows.Forms.GroupBox mrcRegistro;
+        private System.Windows.Forms.NumericUpDown numAnioFabricacion;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.GroupBox mrcConsulta;
+        private System.Windows.Forms.TextBox txtDominio6Caracteres;
+        private System.Windows.Forms.TextBox txtAnioMasAntiguo;
+        private System.Windows.Forms.TextBox txtCantidadTurnos;
+        private System.Windows.Forms.Label lblDominio6Caracteres;
+        private System.Windows.Forms.Label lblAnioMasAntiguo;
+        private System.Windows.Forms.Label lblCantidadTurnos;
+        private System.Windows.Forms.Button btnConsultar;
+    }
+}
+
---
+++ pryHerreraSP3/frmAutoTest.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pryHerreraSP3
+{
+    public partial class frmAutoTest : Form
+    {
+        // declaración de la estructura para los turnos
+        public struct TURNO
+        {
+            public int NumeroTurno;
+            public string Dominio;
+            public int AnioFabricacion;
+            public string Titular;
+        };
+        // constante para la cantidad total de elementos del arreglo
+        const int MAX = 50;
+        // declaración del arreglo unidimensional de 50 elementos
+        public TURNO[] turnos;
+        // variable para controlar la cantidad de elementos cargados
+        public int Cantidad = 0;
+
+        private void limpiarControles()
+        {
+            // valores para el estado inicial de los controles de carga
+            txtNroTurno.Clear();
+            txtDominio.Clear();
+            numAnioFabricacion.Value = 2022;
+            txtTitular.Clear();
+        }
+
+        private void inicializarInterfaz()
+        {
+            limpiarControles(); // estado inicial de los controles de carga de datos
+                                // estado inicial de los controles de consultas
+            txtCantidadTurnos.Clear();
+            txtAnioMasAntiguo.Clear();
+            txtDominio6Caracteres.Clear();
+        }
+
+        private bool validarDatos()
+        {
+            bool resultado = false; // valor a devolver si no se cumplen todas las condiciones
+            // validar la existencia de los datos a ingresar
+            if (txtNroTurno.Text != "" && txtDominio.Text != "" && txtTitular.Text != "")
+            {
+                if (txtDominio.Text.Length >= 6) // validar el contenido del Dominio
+                {
+                    // validar que NO exista el número de turno a cargar
+                    if (!buscarTurno(int.Parse(txtNroTurno.Text)))
+                    {
+                        resultado = true; // si todo está bien devuelve verdadero
+                    }
+                    else
+                    {
+                        MessageBox.Show("El Número de Turno ingresado ya existe",
+                        "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("El Dominio debe tener 6 o 7 caracteres",
+                    "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debe completar los datos faltantes", "ERROR",
+                MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            return resultado;
+        }
+
+
+        // función para buscar un número de turno en el arreglo de turnos
+        // recibe por parámetro el número a buscar
+        // devuelve verdadero si el número existe en el arreglo y falso en caso contrario
+        private bool buscarTurno(int numero)
+        {
+            bool existe = false; // valor inicial a devolver
+            int pos = 0; // primera posición del arreglo
+                         // recorrer el arreglo hasta la posición que tenga datos cargados
+            while (pos < Cantidad)
+            {
+                // comparar el número buscado con el número en el arreglo
+                if (numero == turnos[pos].NumeroTurno)
+                {
+                    existe = true; // indica que el número fue encontrado
+                    break; // sale del ciclo while
+                }
+                pos++; // próxima posición del arreglo
+            }
+            return existe; // devuelve verdadero o falso
+        }
+
+        public frmAutoTest()
+        {
+            InitializeComponent();
+        }
+
+        private void frmAutoTest_Load(object sender, EventArgs e)
+        {
+            // creación del arreglo
+            turnos = new TURNO[MAX];
+            // inicializar la variable que controla la cantidad de elementos cargados
+            Cantidad = 0;
+            // establecer el estado inicial de todos los componentes de la interfaz
+            inicializarInterfaz();
+        }
+
+        private void txtNroTurno_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // sin NO es un dígito y NO es Backspace (para borrar)
+            if (!(Char.IsDigit(e.KeyChar)) && (e.KeyChar != (char)Keys.Back))
+            {
+                e.Handled = true; // borrar la tecla ingresada
+            }
+        }
+
+        private void txtDominio_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // usamos los métodos de la clase 'Char', (IsLower y ToUpper)
+            if (Char.IsLower(e.KeyChar)) // es una minúscula ?
+            {
+                e.KeyChar = Char.ToUpper(e.KeyChar); // convertir a mayúscula
+            }
+            // no es ni letra ni número y es distinto de Backspace ?
+            if (!Char.IsLetterOrDigit(e.KeyChar) && e.KeyChar != (char)Keys.Back)
+            {
+                e.Handled = true; // borrar la tecla
+            }
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            // validar los datos antes de ingresarlos al arreglo
+            if (validarDatos())
+            {
+                // agregar los datos al arreglo en la posición indicada por
+                // la variable 'Cantidad'
+                turnos[Cantidad].NumeroTurno = int.Parse(txtNroTurno.Text);
+                turnos[Cantidad].Dominio = txtDominio.Text;
+                turnos[Cantidad].AnioFabricacion = int.Parse(numAnioFabricacion.Value.ToString());
+                turnos[Cantidad].Titular = txtTitular.Text;
+                // incrementar la cantidad de elementos cargados
+                Cantidad++;
+                // verificar si queda espacio en el arreglo
+                if (Cantidad == MAX)
+                {
+                    MessageBox.Show("Se completó la capacidad de carga de datos",
+                    "ATENCIÓN", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    btnRegistrar.Enabled = false; // deshabilitar el botón Registrar
+                }
+                limpiarControles(); // restablecer el estado inicial del formulario
+            }
+        }
+
+        private void btnConsultar_Click(object sender, EventArgs e)
+        {
+            // primer consulta: cantidad de turnos registrados
+            // se obtiene directamente de la variable 'Cantidad'
+            txtCantidadTurnos.Text = Cantidad.ToString();
+
+            // segunda consulta: se debe recorrer el arreglo y determinar
+            // el menor valor del campo 'AnioFabricacion'
+            int menor = int.MaxValue; // mayor valor posible
+            int pos;
+            // recorrer el arreglo hasta la posición con datos
+            for (pos = 0; pos < Cantidad; pos++)
+            {
+                // comparar el valor del elemento en el arreglo
+                if (turnos[pos].AnioFabricacion < menor)
+                {
+                    menor = turnos[pos].AnioFabricacion; // guarda el menor valor
+                }
+            }
+            // mostrar el resultado
+            txtAnioMasAntiguo.Text = menor.ToString();
+
+            // tercera consulta: cantidad de vehículos con dominio de 6 caracteres
+            int contador = 0; // contador en cero
+                              // recorrer el arreglo hasta la posición con datos
+            for (pos = 0; pos < Cantidad; pos++)
+            {
+                // controlar si la longitud del dominio es 6
+                if (turnos[pos].Dominio.Length == 6)
+                {
+                    contador++; // incrementar el contador
+                }
+            }
+            // mostrar el resultado
+            txtDominio6Caracteres.Text = contador.ToString();
+
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close(); // cierra el formulario
+        }
+    }
+}
+  
